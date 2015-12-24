03ce61345326dbfe08ccfa878158bd2388119fef
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2015-12-24 16:26:52 +0100

    Implement create method in ExercisesController

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,10 +1,12 @@
 class ExercisesController < ApplicationController
   def new
-
+    @form = ExerciseForm.new(params)
   end
 
   def create
-
+    @form = QuoteForm.new(params)
+    return redirect_to exercises_path if @form.save?
+    render :new
   end
 
   def index; end
