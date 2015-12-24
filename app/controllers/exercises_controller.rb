7e5a1155d31ac2a45909d2a0ef28963d375ebdd0
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2015-12-24 17:50:46 +0100

    Move variables to expose in ExercisesController

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,13 +1,14 @@
 class ExercisesController < ApplicationController
-  def new
-    @form = ExerciseForm.new(params)
-  end
+  expose(:form) { ExerciseForm.new(params) }
+  expose(:exercises) { Exercise.all }
+
+  def new; end
 
   def create
-    @form = QuoteForm.new(params)
-    return redirect_to exercises_path if @form.save?
+    return redirect_to exercises_path if form.save
     render :new
   end
 
-  def index; end
+  def index
+  end
 end
