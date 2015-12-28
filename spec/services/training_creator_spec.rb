9faf4c18ec6c0ea48e4964815da487ea8c6471e2
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2015-12-28 18:59:20 +0100

    Add spec for TrainingCreator

---
+++ spec/services/training_creator_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe TrainingCreator do
+  subject { described_class.new(form) }
+  let(:exercises_ids) { [1, 2, 3] }
+  let(:exercises_duration) { [10, 20, 30] }
+  let(:form) do
+    double('form', name: 'some training',
+                   exercises_ids: exercises_ids,
+                   exercises_duration: exercises_duration)
+  end
+
+  it 'creates traning' do
+    expect { subject.call }.to change { Training.all.count }.by(+1)
+  end
+
+  it 'creates trainig_exercises' do
+    expect { subject.call }.to change { TrainingExercise.all.count }.by(+3)
+  end
+
+  it 'creates trainig_exercises with valid duration' do
+    subject.call
+    expect(TrainingExercise.all.map(&:duration)).to eq([10, 20, 30])
+  end
+
+  it 'creates trainig_exercises with valid exercises_ids' do
+    subject.call
+    expect(TrainingExercise.all.map(&:exercise_id)).to eq([1, 2, 3])
+  end
+end
