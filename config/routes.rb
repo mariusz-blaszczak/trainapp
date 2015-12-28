f98ac504d1dbe36ce7b279ec628ce485c4626026
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2015-12-28 17:24:40 +0100

    Add trainings to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :exercises do
     post :record, on: :collection
   end
+  resources :trainings
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
