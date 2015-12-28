bdbcb5bd0333c4d1fee80f0308ef765836b8b818
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2015-12-28 19:07:41 +0100

    Ref TrainingCreator

--- app/services/training_creator.rb
+++ app/services/training_creator.rb
@@ -1,18 +1,29 @@
 class TrainingCreator
   pattr_initialize :form
-  attr_reader :traning
 
   def call
-    @training = Training.create(name: form.name)
-    form.exercises_ids.each_with_index do |exercise_id, index|
-      TrainingExercise.create(
-        exercise_id: exercise_id,
-        duration: form.exercises_duration[index],
-      )
-    end
+    create_training_exercises
   end
 
   private
+
+  def create_training_exercises
+    form.exercises_ids.each_with_index do |exercise_id, index|
+      create_training_exercise(exercise_id, index)
+    end
+  end
+
+  def create_training_exercise(exercise_id, index)
+    TrainingExercise.create(
+      exercise_id: exercise_id,
+      duration: form.exercises_duration[index],
+      training_id: training.id,
+    )
+  end
+
+  def training
+    @training ||= Training.create(name: form.name)
+  end
 
   def exercises
     Exercise.where('ID in (?)', form.exercises_ids)
