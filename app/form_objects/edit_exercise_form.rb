9851e5219712c6a0c53ee763b22e77f9d16070b7
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2015-12-28 15:36:31 +0100

    Add audio for building params of exercise form

--- app/form_objects/edit_exercise_form.rb
+++ app/form_objects/edit_exercise_form.rb
@@ -4,11 +4,12 @@ class EditExerciseForm < ExerciseForm
   def initialize(params, exercise)
     @exercise = exercise
     @params = params
-    super(build_params_from_exercise)
+    super(build_params_from_exercise.compact)
   end
 
   def build_params_from_exercise
     {
+      audio: params[:audio] || exercise.audio || generate_audio_file_name,
       name: params[:name] || exercise.name,
       description: params[:description] || exercise.description
     }
