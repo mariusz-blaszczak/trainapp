6587d29ddc9e2be0f8a34b67c3289d8ba84c1ce8
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2015-12-29 16:39:39 +0100

    Add destroy action

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -1,10 +1,26 @@
 class TrainingsController < ApplicationController
   expose(:form) { TrainingForm.new(params) }
   expose(:trainings) { Training.all }
+  expose(:training) { Training.find(params['id']) }
 
   def new
   end
 
+  def create
+    if form.valid? && TrainingCreator.new(form).call
+      flash[:success] = 'Trening został utworzony'
+      return redirect_to trainings_path
+    end
+    render :new
+  end
+
   def index
   end
+
+  def destroy
+    training.destroy
+    flash[:success] = 'Ćwiczenie zostało usunięte'
+    redirect_to trainings_path
+  end
+
 end
