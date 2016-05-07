f562fb79647a067a378e7783b9b11ab100ea8588
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2016-05-07 16:38:58 +0200

    Add series number to training_form

--- app/form_objects/training_form.rb
+++ app/form_objects/training_form.rb
@@ -7,6 +7,7 @@ class TrainingForm
   attribute :name, String
   attribute :exercises_ids, Array[Integer]
   attribute :exercises_duration, Array[Integer]
+  attribute :exercises_series_number, Array[Integer]
 
   validates :name, presence: true
 end
