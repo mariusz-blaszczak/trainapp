6275986d98fa53b36e8c4b6e4aadbe36dbd71641
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2016-05-07 16:44:23 +0200

    add test validation saving series number

--- spec/services/training_creator_spec.rb
+++ spec/services/training_creator_spec.rb
@@ -4,10 +4,13 @@ describe TrainingCreator do
   subject { described_class.new(form) }
   let(:exercises_ids) { [1, 2, 3] }
   let(:exercises_duration) { [10, 20, 30] }
+  let(:exercises_series_number) { [3, 4, 5] }
   let(:form) do
     double('form', name: 'some training',
-                   exercises_ids: exercises_ids,
-                   exercises_duration: exercises_duration)
+           exercises_ids: exercises_ids,
+           exercises_duration: exercises_duration,
+           exercises_series_number: exercises_series_number,
+    )
   end
 
   it 'creates tracing' do
@@ -26,5 +29,10 @@ describe TrainingCreator do
   it 'creates training_exercises with valid exercises_ids' do
     subject.call
     expect(TrainingExercise.all.map(&:exercise_id)).to eq([1, 2, 3])
+  end
+
+  it 'creates training_exercises with valid series number' do
+    subject.call
+    expect(TrainingExercise.all.map(&:series_number)).to eq([3, 4, 5])
   end
 end
