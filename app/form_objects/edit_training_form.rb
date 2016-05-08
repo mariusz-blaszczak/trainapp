6c1c2464297a8f707296cd6c8d6a211763bf1100
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2016-05-08 09:11:24 +0200

    rubocop fixes

--- app/form_objects/edit_training_form.rb
+++ app/form_objects/edit_training_form.rb
@@ -13,15 +13,15 @@ class EditTrainingForm < TrainingForm
     {
       name: params[:name] || training.name,
       exercises_series_number: params[:exercises_series_number] || exercises_series_numbers,
-      exercises_duration: params[:exercises_duration] || exercises_durations
+      exercises_duration: params[:exercises_duration] || exercises_durations,
     }
   end
 
   def exercises_durations
-    training.training_exercises.map { |e| e.duration }
+    training.training_exercises.map(&:duration)
   end
 
   def exercises_series_numbers
-    training.training_exercises.map { |e| e.series_number }
+    training.training_exercises.map(&:series_number)
   end
 end
